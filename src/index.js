06228c6fbf3d48b5223074d990c15f2d57ad99a0
Dennys Javier Lara Uquillas <dennysjlarau@gmail.com>
2022-10-19 09:53:51 -0500

    prueba

--- src/index.js
+++ src/index.js
@@ -1,10 +1,1 @@
-
-import { heroes } from "./data/heroes";
-
-console.table(heroes);
-
-const getHeroeById = (id) => heroes.find((heroe) => heroe.id === id);
-console.log(getHeroeById(4));
-
-const getHeroeByOwner = (owner) => heroes.filter(heroe => heroe.owner === owner);
-console.table(getHeroeByOwner('Marvel'));
+//
